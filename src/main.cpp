31874c114bfc82031bfc56e99896020349096315
Théo Delas <theo.delas@gmail.com>
2023-01-20 17:36:01 +0100

    Send micro rate with websocket

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,88 @@
+#include "arduinoFFT.h"
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <AsyncTCP.h>
+
+#include <cstdlib> 
+#include <ctime>
+  
+#define SAMPLES 128             //SAMPLES-pt FFT. Must be a base 2 number. Max 128 for Arduino Uno.
+#define SAMPLING_FREQUENCY 1024 //Ts = Based on Nyquist, must be 2 times the highest expected frequency.
+ 
+arduinoFFT FFT = arduinoFFT();
+ 
+unsigned int samplingPeriod;
+unsigned long microSeconds;
+ 
+double vReal[SAMPLES]; //create vector of size SAMPLES to hold real values
+double vImag[SAMPLES]; //create vector of size SAMPLES to hold imaginary values
+
+const char* ssid = "Nokia3310";
+const char* password =  "ynoviotmaster";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+  
+  if(type == WS_EVT_CONNECT){
+  
+    Serial.println("Websocket client connection received");
+     
+  } else if(type == WS_EVT_DISCONNECT){
+ 
+    Serial.println("Client disconnected");
+  
+  }
 }
+  
+void setup(){
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  Serial.println(WiFi.localIP());
+  
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+  
+  server.begin();
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  samplingPeriod = round(1000000*(1.0/SAMPLING_FREQUENCY)); //Period in microseconds 
+}
+  
+void loop(){
+  for(int i=0; i<SAMPLES; i++)
+  {
+      microSeconds = micros(); 
+    
+      vReal[i] = analogRead(35);
+      vImag[i] = 0;
+
+      while(micros() < (microSeconds + samplingPeriod))
+      {
+        //do nothing
+      }
+  }
+
+  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+
+  double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+  Serial.println(peak);
+
+  char str[64];
+  snprintf(str, sizeof(str), "%g", peak);
+
+  Serial.print(str);
+  ws.textAll(str);
+  delay(40); //do one time
 }
