a2668e4ddec4ee7aebf7d70d71ad918552416151
Théo Delas <theo.delas@gmail.com>
2023-01-27 14:01:54 +0100

    WIP note and tuning models

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include "./models/Note.hpp"
+#include "./models/Tuning.hpp"
+#include <string>
+using namespace std;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  note1.frequency = 440.0;
+  note1.letter = 'A';
+  note1.octave = 1;
+  note1.offset = 10;
+
+  Serial.println(note1.isValid(444));
+  Serial.println(note1.isValid(300));
+  delay(500);
 }
